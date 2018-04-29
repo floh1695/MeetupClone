4adc1e803230a575e3ddadcfe8f6f51759d76519
Charles Bayley <cbayley95@gmail.com>
2018-04-29 18:21:10 -0400

    Added an event search that may take in a city and title of the event

---
+++ MeetupClone/Controllers/EventsController.cs
@@ -0,0 +1,40 @@
+﻿using MeetupClone.Contexts;
+using MeetupClone.Models;
+using MeetupClone.Models.In;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MeetupClone.Controllers
+{
+    public class EventsController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult SearchForCities([FromUri]CitySearch citySearch)
+        {
+            var results = new List<Event>();
+            using (var db = new MeetupContext())
+            {
+                var query = db.Events
+                    .Include(event_ => event_.City);
+                if (!string.IsNullOrWhiteSpace(citySearch.City))
+                {
+                    query = query
+                        .Where(event_ => event_.City.Name.Contains(citySearch.City));
+                }
+                if (!string.IsNullOrWhiteSpace(citySearch.Title))
+                {
+                    query = query
+                        .Where(event_ => event_.Title.Contains(citySearch.Title));
+                }
+
+                results.AddRange(query);
+            }
+            return Ok(results);
+        }
+    }
+}
---
+++ MeetupClone/Models/In/CitySearch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MeetupClone.Models.In
+{
+    public class CitySearch
+    {
+        public string City { get; set; }
+        public string Title { get; set; }
+    }
+}
