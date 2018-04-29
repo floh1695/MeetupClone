019544528fd952084959df89f9dab748d398360a
Charles Bayley <cbayley95@gmail.com>
2018-04-29 15:39:56 -0400

    Added Context DbSets

--- MeetupClone/Contexts/MeetupContext.cs
+++ MeetupClone/Contexts/MeetupContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MeetupClone.Models;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -12,5 +13,12 @@ namespace MeetupClone.Contexts
         public MeetupContext()
             : base("name=Meetup")
         { }
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Attendee> Attendees { get; set; }
+
+        public DbSet<Event> Events { get; set; }
+
+        public DbSet<Attendance> Attendance { get; set; }
     }
 }
