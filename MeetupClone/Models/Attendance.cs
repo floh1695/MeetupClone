306a6109ede5321000287b4fd75d5b872d3e679f
Charles Bayley <cbayley95@gmail.com>
2018-04-29 15:35:22 -0400

    Added an Attendance model for joining Attendees and Events

---
+++ MeetupClone/Models/Attendance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MeetupClone.Models
+{
+    public class Attendance
+    {
+        [Key, Column(Order = 0)]
+        public int AttendeeID { get; set; }
+        public Attendee Attendee { get; set; }
+
+        [Key, Column(Order = 1)]
+        public int EventID { get; set; }
+        public Event Event { get; set; }
+    }
+}
