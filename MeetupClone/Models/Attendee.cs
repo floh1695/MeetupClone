b8211477ca01c22bd443a0c81e8fcb619e444263
Charles Bayley <cbayley95@gmail.com>
2018-04-29 15:30:49 -0400

    Added an Attendee model

---
+++ MeetupClone/Models/Attendee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MeetupClone.Models
+{
+    public class Attendee
+    {
+        public int ID { get; set; }
+
+        public string Email { get; set; }
+    }
+}
